adb8942ad8591cd58efaca514bddec82f02b8333
Milos Popovic <milos_popovic@outlook.com>
2020-05-18 06:08:26 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board).even?
-    puts "O"
-  else
+  if turn_count(board) % 2 == 0
     puts "X"
+  else
+    puts "O"
   end
 end
